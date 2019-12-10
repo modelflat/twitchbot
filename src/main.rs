d183631307a17f1806449d2e7d4790ea4a9b6ba0
modelflat <zhykreg@gmail.com>
2019-12-10 18:19:04 +0400

    Remove hard-coded value in main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,24 @@ mod irc;
 
 const TWITCH_IRC_WS: &str = "wss://irc-ws.chat.twitch.tv:443";
 
-
 fn run(username: &str, password: &str, channels: Vec<String>) -> ws::Result<()> {
     ws::connect(TWITCH_IRC_WS, |out| {
         tmi::Client::new(out, username, password, channels.clone()).unwrap()
     })
 }
 
-
 fn main() {
-    let username = "modelflat_bot";
-    let password = env::var("TWITCH_OAUTH_TOKEN").expect("TWITCH_OAUTH_TOKEN should be set!");
-    let channels = vec!["forsen", "xqcow"].iter().map(|x| x.to_lowercase()).collect();
+    let username = env::var("TWITCH_USERNAME")
+        .expect("TWITCH_USERNAME should be set!");
 
-    run(username, &password, channels).unwrap();
+    let password = env::var("TWITCH_OAUTH_TOKEN")
+        .expect("TWITCH_OAUTH_TOKEN should be set!");
+
+    let channels = env::var("TWITCH_CHANNELS_TO_JOIN")
+        .expect("TWITCH_CHANNELS_TO_JOIN should be set!")
+        .split(",")
+        .map(|x| x.to_lowercase())
+        .collect();
+
+    run(&username, &password, channels).unwrap();
 }
