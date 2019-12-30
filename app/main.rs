974be9498930377023b555a5b1370e796e5836b7
modelflat <zhykreg@gmail.com>
2019-12-30 16:01:08 +0400

    Add basic support for command line args

--- app/main.rs
+++ app/main.rs
@@ -1,9 +1,22 @@
 use url::Url;
+use structopt::StructOpt;
 
 mod commands;
 use commands::{commands, permissions, state};
 
+#[derive(StructOpt)]
+#[structopt(about = "primitive twitch bot")]
+struct Opt {
+
+    /// Which channels should the bot join upon startup, comma-separated
+    #[structopt(long)]
+    channels: String,
+
+}
+
 fn main() {
+    let opt: Opt = Opt::from_args();
+
     env_logger::try_init().expect("Failed to initialize logger");
 
     let url = Url::parse("wss://irc-ws.chat.twitch.tv:443").unwrap();
@@ -12,13 +25,11 @@ fn main() {
 
     let password = std::env::var("TWITCH_OAUTH_TOKEN").expect("twitch oauth token");
 
-    let channels = std::env::var("TWITCH_CHANNELS_TO_JOIN").expect("twitch channels to join");
-
     bot::run(
         url,
         username,
         password,
-        channels.split_terminator(',').map(|s| s.to_string()).collect(),
+        opt.channels.split_terminator(',').map(|s| s.to_string()).collect(),
         state(),
         commands(),
         permissions(),
